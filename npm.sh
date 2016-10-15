e9ff441fd364b0809010f58eeea9457c8c85757b
Damiano Oliverio <doliverio@users.noreply.github.com>
2016-10-16 01:02:36 +0200

    Create npm.sh

---
+++ npm.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+npm install -g bootprint
+npm install -g bootprint-swagger
+npm install -g bower
+npm install -g cordova
+npm install -g grunt-cli
+npm install -g gulp
+npm install -g html-inline
+npm install -g ios-deploy
+npm install -g ios-sim
+npm install -g jshint
+npm install -g node-gyp
+npm install -g node-inspector
+npm install -g npm
+npm install -g selenium-standalone
+npm install -g swagger-to-html
+npm install -g yo
